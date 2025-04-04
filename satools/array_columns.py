db4a89289da225501e6a4023dfe900bb7747d860
simonharnqvist <sharnqvi@roe.ac.uk>
2025-04-04 11:54:56 +0100

    full pivot of array cols

--- satools/array_columns.py
+++ satools/array_columns.py
@@ -2,11 +2,27 @@ from pyspark import sql
 from pyspark.sql.functions import collect_list
 from functools import reduce
 
+def transform_passbands(df: sql.DataFrame, filter_col: str = "filterID", new_col_name: str = "passband"):
+    """Convert numeric filter references to letter-encoded passbands"""
+    passbands_dict = {"1": "Z",
+                      "2": "Y",
+                      "3": "J",
+                      "4": "H",
+                      "5": "K",
+                      "8": "blank"}
+    return df.withColumn(new_col_name, df[filter_col].cast("string")).replace(passbands_dict, subset=[new_col_name])
+    
+def _rename_pivoted_columns(df: sql.DataFrame, col_name: str):
+    """Append feature names to passband column names e.g. J -> J_mjd"""
+    old_columns = df.columns[1:]
+    new_columns = [col + f"_{col_name}" for col in old_columns]
+    return reduce(lambda df, idx: df.withColumnRenamed(old_columns[idx], new_columns[idx]), range(len(old_columns)), df)
 
-def aggregate_by_key(grouped_df: sql.group.GroupedData,
-                     col_name: str) -> sql.DataFrame:
-    """Aggregate by key into lists e.g. (sourceID, [val1, val2, val3])"""
-    return grouped_df.agg(collect_list(col_name).alias(col_name))
+def pivot_aggregate_col(grouped_df: sql.group.GroupedData,
+                     col_name: str, pivot_on: str = "passband") -> sql.DataFrame:
+    """Aggregate column by key into lists and pivot on other column e.g. passband"""
+    aggregated = grouped_df.pivot(pivot_on).agg(sql.functions.collect_list(col_name))
+    return _rename_pivoted_columns(aggregated, col_name)
 
 
 def merge_all(aggregated_dfs: list[sql.DataFrame],
@@ -15,18 +31,21 @@ def merge_all(aggregated_dfs: list[sql.DataFrame],
     return reduce(lambda df1, df2: df1.join(df2, on=join_key), aggregated_dfs)
 
 
-def make_array_cols(df: sql.DataFrame, key: str) -> sql.DataFrame:
+def make_array_cols(df: sql.DataFrame, key: str, filter_col:str) -> sql.DataFrame:
     """Transform df to array-valued columns.
 
     Args:
         df (sql.DataFrame): Dataframe in wide format.
         key (str): Unique key to group by.
+        filter_col (str): Column (int) containing passbands to pivot on
 
     Returns:
         sql.DataFrame: Dataframe with array-valued columns.
     """
     assert key in df.columns, f"Key {key} not in columns of dataframe {df}"
+    df = transform_passbands(df, filter_col="filterID", new_col_name = "passband").drop(filter_col)
     grouped = df.groupBy(key)
-    aggregated_dfs = [aggregate_by_key(grouped_df=grouped, col_name=col_name)
-                      for col_name in df.columns if col_name != key]
+    aggregated_dfs = [pivot_aggregate_col(grouped_df=grouped, col_name=col_name, pivot_on="passband")
+                      for col_name in df.columns if col_name not in [key, "passband"]]
     return merge_all(aggregated_dfs, key)
+
--- satools/test_array_columns.py
+++ satools/test_array_columns.py
@@ -1,7 +1,7 @@
 import pytest
 from pyspark.testing import assertDataFrameEqual
 from pyspark.sql import SparkSession
-from array_columns import aggregate_by_key, merge_all, make_array_cols
+from array_columns import transform_passbands, pivot_aggregate_col, merge_all, make_array_cols
 
 
 @pytest.fixture
@@ -11,33 +11,63 @@ def spark_context():
 
 @pytest.fixture
 def sample_data(spark_context):
+    return spark_context.createDataFrame(
+    [
+        (1, 1, 1.234, 2.567),
+        (1, 2, 3.234, 4.567),
+        (1, 3, 5.234, 6.567),
+        (1, 4, 7.234, 8.567),
+        (1, 5, 9.234, 10.567),
+        (2, 1, 1.234, 2.567),
+        (2, 2, 3.234, 4.567),
+        (2, 3, 5.234, 6.567),
+        (2, 4, 7.234, 8.567),
+        (2, 5, 9.234, 10.567),
+        (3, 1, 1.234, 2.567),
+        (3, 2, 3.234, 4.567),
+        (3, 3, 5.234, 6.567),
+        (3, 4, 7.234, 8.567),
+        (3, 5, 9.234, 10.567)],
+    ["sourceID", "filterID", "mjd", "aperMag1"])
 
-    df = spark_context.createDataFrame(
-        [
-            (1, 0.1, 0.2, 0.3),
-            (1, 0.5, 0.6, 0.7),
-            (2, 0.1, 0.2, 0.3),
-            (2, 0.9, 1.1, 1.2),
-        ],
-        ["id", "col1", "col2", "col3"]
-    )
 
-    return df
+def test_transform_passbands(sample_data, spark_context):
 
-
-def test_aggregate_by_key(spark_context, sample_data):
-
+    actual = transform_passbands(sample_data, filter_col="filterID", new_col_name="passband")
     expected = spark_context.createDataFrame(
-        [(1, [0.1, 0.5]),
-         (2, [0.1, 0.9])],
-        ["id", "col1"]
-    )
-
-    grouped = sample_data.groupBy("id")
-    actual = aggregate_by_key(grouped, "col1")
+        [
+        (1, 1, 1.234, 2.567, "Z"),
+        (1, 2, 3.234, 4.567, "Y"),
+        (1, 3, 5.234, 6.567, "J"),
+        (1, 4, 7.234, 8.567, "H"),
+        (1, 5, 9.234, 10.567, "K"),
+        (2, 1, 1.234, 2.567, "Z"),
+        (2, 2, 3.234, 4.567, "Y"),
+        (2, 3, 5.234, 6.567, "J"),
+        (2, 4, 7.234, 8.567, "H"),
+        (2, 5, 9.234, 10.567, "K"),
+        (3, 1, 1.234, 2.567, "Z"),
+        (3, 2, 3.234, 4.567, "Y"),
+        (3, 3, 5.234, 6.567, "J"),
+        (3, 4, 7.234, 8.567, "H"),
+        (3, 5, 9.234, 10.567, "K")],
+    ["sourceID", "filterID", "mjd", "aperMag1", "passband"])
 
     assertDataFrameEqual(actual, expected)
 
+def test_pivot_aggregate_columns(sample_data, spark_context):
+    df = transform_passbands(sample_data, filter_col="filterID", new_col_name="passband")
+    grouped = df.groupBy("sourceID")
+    actual = pivot_aggregate_col(grouped, col_name="mjd", pivot_on="passband")
+    expected = spark_context.createDataFrame(
+    [
+        (1, [7.234], [5.234], [9.234], [3.234], [1.234]),       
+        (2, [7.234], [5.234], [9.234], [3.234], [1.234]),
+        (3, [7.234], [5.234], [9.234], [3.234], [1.234])],
+    ["sourceID", "H_mjd", "J_mjd", "K_mjd", "Y_mjd", "Z_mjd"]
+)
+    
+    assertDataFrameEqual(actual, expected)
 
 def test_merge_all(spark_context, sample_data):
 
@@ -57,11 +87,13 @@ def test_merge_all(spark_context, sample_data):
     assertDataFrameEqual(merged, agg_df)
 
 
+
+
 def test_make_array_cols(spark_context, sample_data):
     expected = spark_context.createDataFrame(
-        [(1, [0.1, 0.5], [0.2, 0.6], [0.3, 0.7]),
-         (2, [0.1, 0.9], [0.2, 1.1], [0.3, 1.2])],
-        ["id", "col1", "col2", "col3"]
-    )
-    actual = make_array_cols(sample_data, key="id")
+    [(1, [7.234], [5.234], [9.234], [3.234], [1.234], [8.567], [6.567], [10.567], [4.567], [2.567]),
+     (2, [7.234], [5.234], [9.234], [3.234], [1.234], [8.567], [6.567], [10.567], [4.567], [2.567]),
+     (3, [7.234], [5.234], [9.234], [3.234], [1.234], [8.567], [6.567], [10.567], [4.567], [2.567])],
+     ["sourceID", "H_mjd", "J_mjd", "K_mjd", "Y_mjd", "Z_mjd", "H_aperMag1", "J_aperMag1", "K_aperMag1", "Y_aperMag1", "Z_aperMag1"])
+    actual = make_array_cols(sample_data, key="sourceID", filter_col="filterID")
     assertDataFrameEqual(actual, expected)
