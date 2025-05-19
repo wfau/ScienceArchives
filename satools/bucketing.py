1631d8ac2544c2aaaa7df423b17a3ebf2ee82772
simonharnqvist <sharnqvi@roe.ac.uk>
2025-05-19 11:25:50 +0100

    prevent SQL injection

--- satools/bucketing.py
+++ satools/bucketing.py
@@ -1,4 +1,24 @@
-def bucket_save(df, buckets, key, table_name, spark):
+import re
+from pyspark.sql import DataFrame, SparkSession
+
+
+def _sanitize_identifier(identifier: str):
+    """Prevent SQL injection by ensuring identifier does not contain dangerous chars"""
+    if re.match(r"^[A-Za-z_][A-Za-z0-9_]*$", identifier):
+        return identifier
+    else:
+        raise ValueError(f"Invalid SQL identifier: {identifier}")
+
+
+def bucket_save(
+    df: DataFrame, buckets: int, key: str, table_name: str, spark: SparkSession
+):
+
+    key = _sanitize_identifier(key)
+    table_name = _sanitize_identifier(table_name)
+    if not isinstance(int, buckets) and not buckets > 0:
+        raise ValueError("Buckets must be integer > 0")
+
     df = df.repartition(buckets, key)
     df.createOrReplaceTempView("tmp_view")
     spark.sql(
