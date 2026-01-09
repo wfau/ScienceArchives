d59fe84ab495fc95933f7ad959c455cd6d695f73
Amy Krause <a.krause@epcc.ed.ac.uk>
2026-01-09 15:40:32 +0000

    create new user if remote user email does not exist

--- web/moons/server/core/backends.py
+++ web/moons/server/core/backends.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.backends import RemoteUserBackend
 from django.contrib.auth import get_user_model
 
+import logging
+logger = logging.getLogger(__name__)
+
 UserModel = get_user_model()
 
 class MyRemoteUserBackend(RemoteUserBackend):
@@ -11,9 +14,8 @@ class MyRemoteUserBackend(RemoteUserBackend):
         # we are not creating new users
         # only identify users by email
         user = None
-        try:
-            email = UserModel._default_manager.normalize_email(remote_user)
-            user = UserModel.objects.get(email=email)
-        except UserModel.DoesNotExist:
-            pass
+        email = UserModel._default_manager.normalize_email(remote_user)
+        user, created = UserModel._default_manager.get_or_create(email=email)
+        if created:
+            logger.info(f'New user: "{email}"')
         return user if super().user_can_authenticate(user) else None
