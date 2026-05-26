01f158c072fcff6e8d7ccf927af2c43c156fb9bf
Amy Krause <a.krause@epcc.ed.ac.uk>
2026-05-26 20:37:11 +0100

    update node version and pnpm

--- web/moons/docker/production/frontend/build-prod.sh
+++ web/moons/docker/production/frontend/build-prod.sh
@@ -5,8 +5,11 @@ corepack enable
 cp -r /mnt/web-ui /
 
 cd /web-ui
+rm -r node_modules
+rm -r dist
 
 pnpm install --frozen-lockfile
+pnpm approve-builds esbuild
 pnpm run build
 
 cp -r /web-ui/dist /mnt/web-ui/
