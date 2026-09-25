17f447fd3e9cffd4e0b5d360d25a7b22cf7f8c23
Amy Krause <a.krause@epcc.ed.ac.uk>
2026-09-25 19:09:45 +0100

    helper page for using CILogon

--- web/moons/server/core/urls.py
+++ web/moons/server/core/urls.py
@@ -6,4 +6,5 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("login/", views.LoginInfoView.as_view(), name="login-info"),
 ]
--- web/moons/server/core/views.py
+++ web/moons/server/core/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+class LoginInfoView(TemplateView):
+    template_name = 'core/login-info.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.COOKIES.get('skip_login_info'):
+            return HttpResponseRedirect('/frontend/')
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        response = HttpResponseRedirect('/frontend/')
+        if request.POST.get('skip') == '1':
+            response.set_cookie('skip_login_info', '1', max_age=31536000, path='/')
+        return response
